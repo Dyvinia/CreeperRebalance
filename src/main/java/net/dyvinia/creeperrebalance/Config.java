a4d069f4ebfacddb2f3210885be487317c96ddd4
Dyvinia <dulana57@gmail.com>
2024-11-23 14:08:15 -0500

    update defaults to make more sense with the updated math

--- src/main/java/net/dyvinia/creeperrebalance/Config.java
+++ src/main/java/net/dyvinia/creeperrebalance/Config.java
@@ -16,16 +16,16 @@ public class Config {
             .comment("\n Multiplier for extra knockback caused by the explosion.\n Set to 0.0 for vanilla behavior.\n Default: 4.0")
             .define("knockbackMult", 4.0);
     private static final ForgeConfigSpec.ConfigValue<Double> PLAYER_KNOCKBACK_MULT = BUILDER
-            .comment("\n Multiplier for extra knockback caused by the explosion to players.\n Set to -1.0 to default to the value of knockbackMult.\n Default: 3.0")
-            .define("playerKnockbackMult", 3.0);
+            .comment("\n Multiplier for extra knockback caused by the explosion to players.\n Set to -1.0 to default to the value of knockbackMult.\n Default: 3.5")
+            .define("playerKnockbackMult", 3.5);
 
     private static final ForgeConfigSpec.ConfigValue<Double> KNOCKBACK_UP = BUILDER
             .comment("\n Adjusts the upward velocity added by the explosion as part of its knockback.\n Default: 0.1")
             .define("knockbackUp", 0.1);
 
     private static final ForgeConfigSpec.ConfigValue<Double> FALLOFF_EXPONENT = BUILDER
-            .comment("\n Exponent used for calculating the falloff.\n Default: 2.0")
-            .define("falloffExponent", 2.0);
+            .comment("\n Exponent used for calculating the falloff.\n Default: 2.5")
+            .define("falloffExponent", 2.5);
 
     public static boolean disableGriefing;
     public static double knockbackMult;
